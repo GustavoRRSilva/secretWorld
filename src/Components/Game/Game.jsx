8e62b37c8081be592a21a7efd5b947cdd2361e8a
Gustavo <gstvrrds@gmail.com>
2024-03-12 17:15:51 -0300

    Feat:Css da página game

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import './Game.css'
 export const Game = ({verifyLetter}) => {
   return (
-    <div>
-        <h2 className='title'>Game</h2>
-        <button onClick = {verifyLetter}> Game over</button>
+    <div className='Name'>
+        <p className="points">
+          <span>Pontuação: 000</span>
+        </p>
+        <h1>Adivinhe a palavra:</h1>
+        <h3 className="tip">
+          Dica sobre a palavra: <span>Dica...</span>
+        </h3>
+        <div className="wordContainer">
+          <span className="letter">A</span>
+          <span className="blankSquare"></span>
         </div>
+        <div className="letterContainer">
+          <p>Tente adivinhar uma letra da palavra:</p>
+          <form action="">
+            <input type="text" name="letter" maxLength = "1"  required/>
+            <button>Jogar</button>
+          </form>
+        </div>
+        <div className="wrongLettersContainer">
+          <p>Letras já utilizadas:</p>
+          <span>a,</span>
+          <span>b,</span>
+        </div>
+    </div>
   )
 }
